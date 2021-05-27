22848e6c1ad806ad36410e1bb56198cf4f843e19
VIKAS P LAL <49762419+vikasm19@users.noreply.github.com>
2021-05-27 11:20:39 +0530

    Add files via upload

---
+++ hacker-rank-q-2-c++-langauge.cpp
@@ -0,0 +1,15 @@
+// Hacker-Rank 
+// c++ Language 
+
+// Q-2 
+// Input & Output
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c; //Declaring the variables.
+    cout<<"Enter any three numbers of your choice: "<<"\n";
+    cin>>a>>b>>c;  //Taking User Input;
+    cout<<"Sum of your numbers is: "<<a+b+c<<endl;   //Printing teh required output;
+    return 0;
+}
